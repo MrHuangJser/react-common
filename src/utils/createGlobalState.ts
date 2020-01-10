6eb1e5c5da7162876ff946e6abb451bd5be54200
MrHuangJser <565628147@qq.com>
2020-01-10 13:42:28 +0800

    MrHuangJser - perfect create global state types

--- src/utils/createGlobalState.ts
+++ src/utils/createGlobalState.ts
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 export function createGlobalState<T = any>(initialState?: T) {
-  const store: { state: T | undefined; setState: (v: T) => void; setters: any[] } = {
+  const store: { state: T | undefined; setState: Dispatch<SetStateAction<T>>; setters: any[] } = {
     state: initialState,
     setState(val) {
-      store.state = val;
+      store.state = val instanceof Function ? val(store.state!) : val;
       store.setters.forEach(setter => setter(store.state));
     },
     setters: [],
   };
-  return (): [T | undefined, (v: T) => void] => {
+  return (): [T | undefined, Dispatch<SetStateAction<T>>] => {
     const [state, set] = useState(store.state);
 
     if (!store.setters.includes(set)) {
